10f929ad1816b47a04644c5603d8e0098114af82
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-07-08 23:36:55 -0300

    Aula 4 - Classe DAL Usuario - Método incluir

---
+++ DAL/DALUsuario.cs
@@ -0,0 +1,33 @@
+﻿using Modelo;
+using System;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DALUsuario
+    {
+        private DALConexao conexao;
+
+        public DALUsuario(DALConexao cx)
+        {
+            conexao = cx;
+        }
+
+        public void Incluir( Usuario usuario )
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao.ObjetoConexao;
+            cmd.CommandText = "insert into usuario(use_nome,use_login,use_grupo,use_senha) values"+
+                               "(@use_nome,@use_login,@use_grupo,@use_senha); select @@IDENTITY;";
+            cmd.Parameters.AddWithValue("@use_nome", usuario.Nome);
+            cmd.Parameters.AddWithValue("@use_login", usuario.Login);
+            cmd.Parameters.AddWithValue("@use_grupo", usuario.Grupo);
+            cmd.Parameters.AddWithValue("@use_senha", usuario.Senha);
+            conexao.Conectar();
+            usuario.Id = Convert.ToInt32(cmd.ExecuteScalar());
+            conexao.Desconectar();
+          
+        }
+        
+    }
+}
