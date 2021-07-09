93b21b7589ac361bbe5e2f9f3e75c1138d9811ce
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-07-08 22:48:01 -0300

    Aula 3 - Criação da classe usuário no projeto Modelo

---
+++ Modelo/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string  Grupo { get; set; }
+        public string Senha { get; set; }
+
+        public Usuario()
+        {
+            Id = 0;
+            Nome = "";
+            Login = "";
+            Grupo = "";
+            Senha = "";
+
+        }
+
+        public Usuario(int id, string nome, string login, string grupo, string senha)
+        {
+            Id = id;
+            Nome = nome;
+            Login = login;
+            Grupo = grupo;
+            Senha = senha;
+
+        }
+    }
+}
