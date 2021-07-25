f8c81b9f9bad74e0213a8f5f15ffdcd612b0654c
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-07-25 19:33:14 -0300

    Aula 12 - Layout do formulário pesquisar usuário.

---
+++ GUI/frmLocalizarUsuario.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GUI
+{
+    partial class frmLocalizarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.ColumnHeadersVisible = false;
+            this.dgvDados.Location = new System.Drawing.Point(12, 92);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.ReadOnly = true;
+            this.dgvDados.RowTemplate.Height = 28;
+            this.dgvDados.Size = new System.Drawing.Size(904, 380);
+            this.dgvDados.TabIndex = 0;
+            this.dgvDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(48, 51);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(709, 26);
+            this.txtValor.TabIndex = 1;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Location = new System.Drawing.Point(771, 51);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(132, 26);
+            this.btnLocalizar.TabIndex = 2;
+            this.btnLocalizar.Text = "Localizar";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome ou Login do Usuário";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmLocalizarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.dgvDados);
+            this.Name = "frmLocalizarUsuario";
+            this.Text = "Consultar Usuários Cadastrados";
+            this.Load += new System.EventHandler(this.frmLocalizarUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnLocalizar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ GUI/frmLocalizarUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmLocalizarUsuario : Form
+    {
+        public frmLocalizarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLocalizarUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
